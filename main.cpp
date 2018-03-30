f7c985c6c7b14ddd95bf20903671caacf972718b
Blefler <blefler@mail.csuchico.edu>
2018-03-29 18:38:43 -0700

    Fixed

--- main.cpp
+++ main.cpp
@@ -6,22 +6,37 @@ using namespace std;
 
 int main()
 {
-  string line;
   char first = ' ', last = ' ', current;
+  char letter;
   ifstream myfile;
   myfile.open ("example.txt", ios::in);
-  myfile.seekg (0, ios::beg);
-  myfile>>first;
- // first = 
-  myfile.seekg (-2, ios::end);
-  myfile>>last;
-
+  while (!myfile.eof())
+    {
+      if (first==' ')
+        {
+         myfile>>letter;
+           if ((letter<91&&letter>64)||(letter>96&&letter<123))
+             {
+             first=letter;
+             }
+        }
+      else
+        {
+         myfile>>letter;
+         if ((letter<91&&letter>64)||(letter>96&&letter<123))
+          {
+          last=letter;
+          }
+        }
+     } 
+ // myfile.seekg (0, ios::beg);
+ // myfile>>first; 
+ // myfile.seekg (offset, ios::end);
+ // myfile>>last;
+     
   //Read from the file opened by in, and the first letter (either upper or
   //lower case) should be assigned to first and the last letter (again in
   //either case) from the file to the variable last
-
-//F**
-//F**k
 
   //Then, finally, this displays the censored version:
   cout<<first<<"**"<<last<<endl;
